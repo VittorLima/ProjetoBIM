4830e6a9fcc5057c2785660096e4223696632750
Vittor Lima <vittorlima@discente.ufg.br>
2025-09-23 23:46:00 -0300

    feat(ar): HTTPS no dev (Vite), setWasmPath(BASE_URL) e melhorias AR

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  base: "ProjetoBIM"
-});
+  plugins: [basicSsl()],
+  server: {
+    https: true,       // ativa https
+    host: true,        // 0.0.0.0 (expor na rede)
+    port: 5173,
+    strictPort: true
+  },
+  // para GitHub Pages:
+  // base: '/ProjetoBIM/'
+})
