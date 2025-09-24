d915ceef3aba9901814ac22bdcc8b04ef29c042f
Vittor Lima <vittorlima@discente.ufg.br>
2025-09-23 23:57:47 -0300

    correcao config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from '@vitejs/plugin-basic-ssl' // ok deixar no dev
 
 export default defineConfig({
   plugins: [basicSsl()],
-  server: {
-    https: true,       // ativa https
-    host: true,        // 0.0.0.0 (expor na rede)
-    port: 5173,
-    strictPort: true
-  },
-  // para GitHub Pages:
-  // base: '/ProjetoBIM/'
+  server: { https: true, host: true, port: 5173, strictPort: true },
+  base: '/ProjetoBIM/'           // nome do repositório
 })
